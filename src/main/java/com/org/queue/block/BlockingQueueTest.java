28a7662f61091b1619649eb77906c087a04e0a0b
xudongdong <xudongdong@jianlc.com>
2019-11-28 20:11:32 +0800

    NIO学习

--- src/main/java/com/org/queue/block/BlockingQueueTest.java
+++ src/main/java/com/org/queue/block/BlockingQueueTest.java
@@ -8,10 +8,27 @@
 
 package com.org.queue.block;
 
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * @author:xudd
  * @date:2019/11/12 -17:24
  * @desc:
  **/
 public class BlockingQueueTest {
+    public static void main(String[] args) {
+        Collection c;
+        Map map;
+        List list;
+        Set set;
+        Queue queue;
+        AbstractMap map1;
+        SortedMap sortedMap;
+        HashMap hashMap;
+        TreeMap treeMap;
+        IdentityHashMap identityHashMap;
+        BlockingQueue blockingQueue= new ArrayBlockingQueue(10);
+    }
 }
