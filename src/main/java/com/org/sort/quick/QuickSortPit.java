6fcdea3cc939a928dd5a7c8a91e4a162accc599c
xudongdong <xudongdong@jianlc.com>
2020-06-05 14:48:20 +0800

    快速排序处理

---
+++ src/main/java/com/org/sort/quick/QuickSortPit.java
@@ -0,0 +1,34 @@
+/*
+ *company:jlc
+ *author:xudd
+ *date:2020/6/5:14:44
+ *desc:{}
+ **/
+
+
+package com.org.sort.quick;
+
+/**
+ * @author:xudd
+ * @date:2020/6/5 -14:44
+ * @desc: 快速排序填坑法
+ **/
+public class QuickSortPit {
+
+/**基本思路
+ *
+ *  设定基准pivot 选最左边元素为基准值
+ *
+ *
+ *
+ *
+ * **/
+
+
+
+
+
+
+
+
+}
