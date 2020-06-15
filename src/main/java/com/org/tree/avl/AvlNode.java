c406941bcedf9ba3a5cabbc8064be5140603b140
xudongdong <1107582053@qq.com>
2020-06-15 21:54:54 +0800

    AVL树简单推理，后续还需要深入研究

--- src/main/java/com/org/tree/avl/AvlNode.java
+++ src/main/java/com/org/tree/avl/AvlNode.java
@@ -1,46 +1,19 @@
 package com.org.tree.avl;
-
-import lombok.Data;
-
 /**
- *
- * AVL树的节点
+ * AVL节点
  *
  * */
-@Data
 public class AvlNode {
 
     int data;
+
     /**左孩子*/
-   private AvlNode lchild;
+    AvlNode lchild;
+
+
     /**右孩子*/
-   private   AvlNode rchild;
-     /**记录节点的高度*/
-  private   int height ;
-
-
-     /**
-      * @author: xudd
-      * @desc: 获取树的高度 【递归方法】
-      * @date: 19:42 2020/6/15
-      * @param:
-      * @return:
-      **/
-    public int getTreeHeight(){
-
-       int leftHeight=0;
-       int rightHeight=0;
-       //返回左子树高度
-       if(lchild!=null){
-           leftHeight=lchild.getTreeHeight();
-       }
-       //返回右子树高度
-       if(rchild!=null){
-           rightHeight=rchild.getTreeHeight();
-       }
-        return Math.max(leftHeight,rightHeight)+1;
-
-    }
-
+    AvlNode rchild;
+    /**节点高度*/
+    int height ;
 
 }
