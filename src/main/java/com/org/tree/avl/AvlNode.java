87cedf1ec57f38fb641a078de2cef833c25cb995
xudongdong <xudongdong@jianlc.com>
2020-06-15 19:42:54 +0800

    AVL tree

--- src/main/java/com/org/tree/avl/AvlNode.java
+++ src/main/java/com/org/tree/avl/AvlNode.java
@@ -1,19 +1,49 @@
 package com.org.tree.avl;
 
+import lombok.Data;
+
 /**
  *
  * AVL树的节点
  *
  * */
+@Data
 public class AvlNode {
 
     int data;
     /**左孩子*/
-    AvlNode lchild;
+   private AvlNode lchild;
     /**右孩子*/
-    AvlNode rchild;
+   private   AvlNode rchild;
      /**记录节点的高度*/
-    int height ;
+  private   int height ;
+
+
+     /**
+      * @author: xudd
+      * @desc: 获取树的高度
+      * @date: 19:42 2020/6/15
+      * @param:
+      * @return:
+      **/
+    public int getTreeHeight(){
+
+       int height= 0;
+       int leftHeight=0;
+       int rightHeight=0;
+       //返回左子树高度
+       if(lchild!=null){
+           leftHeight=lchild.getTreeHeight();
+       }
+       //返回右子树高度
+       if(rchild!=null){
+           rightHeight=rchild.getTreeHeight();
+       }
+       height=Math.max(leftHeight,rightHeight)+1;
+
+       return height;
+
+    }
 
 
 }
